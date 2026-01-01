49c68cb874dc6fdfe88c8a3cba8c5aa9baed2633
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2026-01-02 00:11:22 +0530

    change task index.js

--- task/index.js
+++ task/index.js
@@ -5,41 +5,41 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 
-// 🚨 Set same secret for Tasks & NGINX later
 const SECRET = "SUPER_SECRET_KEY";
 
-// 📌 Connect to MongoDB (inside docker)
 mongoose.connect(process.env.MONGO_URL || "mongodb+srv://utkarshsingh500500:AvNQD31mJn4ATxf2@cluster0.zhpzk7l.mongodb.net/taskapp?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("task DB Connected"))
   .catch(err => console.error("Mongo Error:", err));
 
-
 const taskSchema = new mongoose.Schema({
-   title: String,
-   user: String
+  title: String,
+  user: String
 });
 const Task = mongoose.model("Task", taskSchema);
 
-
 function auth(req,res,next){
-    try {
-        const token=req.headers.authorization?.split(" ")[1];
-        const decode=jwt.verify(token,SECRET)
-        req.user=decode.email
-        next()
-        
-    } catch {
-            res.status(403).send("Not authorized");
-
-    }
+  try {
+      const token = req.headers.authorization?.split(" ")[1];
+      const decode = jwt.verify(token,SECRET);
+      req.user = decode.email;
+      next();
+  } catch {
+      res.status(403).send("Not authorized");
+  }
 }
 
+app.get("/tasks", auth, getTasks);
+app.get("/tasks/", auth, getTasks);
 
-app.get("/tasks",auth,async(req,res)=>{
-    const task=await Task.find({user:req.user})
-    res.json(task)
-})
-
+async function getTasks(req, res) {
+  try {
+    const tasks = await Task.find({ user: req.user });
+    res.json(tasks);
+  } catch (err) {
+    console.error("TASK GET ERROR:", err);
+    res.status(500).send("Internal Server Error ❌ GET");
+  }
+}
 
 app.post("/tasks", auth, handleCreate);
 app.post("/tasks/", auth, handleCreate);
@@ -49,37 +49,9 @@ async function handleCreate(req, res) {
     const task = await Task.create({ title: req.body.title, user: req.user });
     res.json(task);
   } catch (err) {
-    console.log("TASK ERROR:", err);
-    res.status(500).send("Task Creation Failed ❌");
+    console.error("TASK CREATE ERROR:", err);
+    res.status(500).send("Internal Server Error ❌ POST");
   }
 }
 
-
-
-
-
-
-// app.post("/tasks", auth, async (req, res) => {
-//   const task = await Task.create({ title: req.body.title, user: req.user });
-//   res.json(task);
-// });
-
-
 app.listen(4001, () => console.log("Task service running 4001🚀"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
