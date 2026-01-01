433e4425865fe6e9e9fff954795a50d4914c855b
utkarsh singh <121781121+utkarsh2004-07@users.noreply.github.com>
2026-01-01 23:31:27 +0530

    Fix task model + query

--- task/index.js
+++ task/index.js
@@ -10,7 +10,7 @@ const SECRET = "SUPER_SECRET_KEY";
 
 // 📌 Connect to MongoDB (inside docker)
 mongoose.connect(process.env.MONGO_URL || "mongodb+srv://utkarshsingh500500:AvNQD31mJn4ATxf2@cluster0.zhpzk7l.mongodb.net/taskapp?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("Auth DB Connected"))
+  .then(() => console.log("task DB Connected"))
   .catch(err => console.error("Mongo Error:", err));
 
 
@@ -18,7 +18,7 @@ const taskSchema = new mongoose.Schema({
    title: String,
    user: String
 });
-const Task = mongoose.model("User", taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
 
 function auth(req,res,next){
@@ -36,7 +36,7 @@ function auth(req,res,next){
 
 
 app.get("/tasks",auth,async(req,res)=>{
-    const task=await Task.findOne({user:req.user})
+    const task=await Task.find({user:req.user})
     res.json(task)
 })
 
